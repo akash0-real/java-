6458832abb571eb0be5eab6b0e8d1cb7c0261ece
akash <aayush45r@gmail.com>
2025-08-21 00:23:56 +0530

    OOPS Car

---
+++ oops/first.java
@@ -0,0 +1,27 @@
+package oops;
+public class first{
+    public static void main(String[] args){
+        Car one = new Car("Audi",4,"red");
+        one.result();
+    }
+
+}
+
+class Car{
+    String name;
+    int seats;
+    String color;
+
+    Car(String name,int  seats,String color) {
+        this.name = name;
+        this.seats = seats;
+        this.color = color;
+    }
+
+    void result(){
+        System.out.println("The name name is " + name + ". Seats: " + seats + " and color: " + color);
+    }
+    
+    
+
+}
