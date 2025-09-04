1a1bcaf5b348255295e639d4a2e1d5c233b4a94a
akash <aayush45r@gmail.com>
2025-09-04 22:47:59 +0530

    using packages

---
+++ connect/greeting_2.java
@@ -0,0 +1,10 @@
+package connect;
+
+public class greeting_2 {
+    public static void main(String[] args) {
+        
+    }
+    public static void message() {
+        System.out.println("hello from 2nd");
+    }
+}
