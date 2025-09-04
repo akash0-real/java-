9ac1ceee7212399778e944ae46e343720aaf618f
akash <aayush45r@gmail.com>
2025-09-04 22:49:54 +0530

    using packages

---
+++ connect/greeting.java
@@ -0,0 +1,10 @@
+package connect;
+import static connect.greeting_2.message;
+
+public class greeting{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        message();
+    }
+
+}
