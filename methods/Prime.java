4091249366544f85b26abfa7058c7a8230936100
akash <aayush45r@gmail.com>
2025-09-06 21:46:26 +0530

    prime number using methods

---
+++ methods/Prime.java
@@ -0,0 +1,14 @@
+package methods;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        System.out.print("enter a number: ");
+        num = scanner.nextInt();
+        
+        scanner.close();
+
+    }
+    static
+}
