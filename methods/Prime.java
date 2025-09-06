3885cf8f2abbdaca369e333fe0a452f2763259ec
akash <aayush45r@gmail.com>
2025-09-06 21:57:11 +0530

    updated

--- methods/Prime.java
+++ methods/Prime.java
@@ -7,8 +7,26 @@ public class Prime {
         System.out.print("enter a number: ");
         num = scanner.nextInt();
         
+        one(num);
+        
         scanner.close();
 
     }
-    static
+    static void one(int a){
+        if(a==1){
+            System.out.println("it is not prime!");
+        }
+        if(a==2){
+            System.out.println("it is prime!");
+        }
+        for(int i=2;i<=Math.sqrt(a);i++){
+            if(a%i==0){
+                System.out.println("it is not prime number!");
+                break;
+            }
+            else{
+                System.out.println("it is prime!");
+            }
+        }
+    }
 }
