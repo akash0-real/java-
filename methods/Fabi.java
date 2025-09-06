68d0648c7146143c42db32b4701b76c10845507f
akash <aayush45r@gmail.com>
2025-09-06 22:12:55 +0530

    fabinacci using methods

---
+++ methods/Fabi.java
@@ -0,0 +1,26 @@
+package methods;
+import java.util.Scanner;
+
+public class Fabi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int number = scanner.nextInt();
+        fabinacci(number);
+
+
+    }
+    static void  fabinacci(int a){
+        int c = 0;
+        int b = 1;
+        for(int i = 0;i<a;i++){
+            System.out.print(c + " ");
+            int sum = c+b;
+            c=b;
+            b= sum;
+        }
+
+
+    }
+}
+
