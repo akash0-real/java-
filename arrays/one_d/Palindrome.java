f80cea8f9cdcae92f3ab782bc5040f5dc8690164
akash <aayush45r@gmail.com>
2025-09-13 01:43:49 +0530

    Palindrome

---
+++ arrays/one_d/Palindrome.java
@@ -0,0 +1,65 @@
+package arrays.one_d;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Check one = new Check(scanner);
+        one.va(scanner);
+        one.Pal();
+        scanner.close();
+    }
+}
+
+class Arr {
+
+    int choice;
+    int[] arr1;
+
+    Arr(Scanner scanner) {
+        System.out.print("Enter the size of the array: ");
+        this.choice = scanner.nextInt();
+        this.arr1 = new int[choice];
+    }
+}
+
+class Input extends Arr {
+
+    Input(Scanner scanner) {
+        super(scanner);
+    }
+
+    void va(Scanner scanner) {
+        for (int i = 0; i < choice; i++) {
+            System.out.print("Enter the values in array: ");
+            arr1[i] = scanner.nextInt();
+        }
+    }
+}
+
+class Check extends Input {
+
+    boolean run = false;
+
+    Check(Scanner scanner) {
+        super(scanner);
+    }
+
+    void Pal() {
+        for (int i = 0; i < choice; i++) {
+            for (int j = arr1.length - 1; j >= 0; j++) {
+                if (arr1[i] == arr1[j]) {
+                    run = true;
+                }
+
+            }
+        }
+        if (!run) {
+            System.out.println("the array is palindrome!");
+        } else {
+            System.out.println("it is not");
+        }
+    }
+}
