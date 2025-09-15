3d63ae31c08efe63b5d126848846d42e92b22be0
akash <aayush45r@gmail.com>
2025-09-15 16:21:45 +0530

    simple arraylist program

--- arrays/arr_list/First.java
+++ arrays/arr_list/First.java
@@ -1,9 +1,18 @@
 package arrays.arr_list;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class First {
    public static void main(String[] args) {
     try(Scanner scanner = new Scanner(System.in)){
-        
+        ArrayList<Integer> one = new ArrayList<>(10);
+        for(int i = 0;i<5;i++){
+            one.add(scanner.nextInt());
+        }
+        for(int i = 0;i<5;i++){
+            System.out.print(one.get(i) + " ");
+        }
+        System.out.println(one);
+
     }
    } 
 }
