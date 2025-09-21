a9c4c233efa254da1a6bcffba923c03b35387516
akash <aayush45r@gmail.com>
2025-09-21 22:01:18 +0530

    runtime polymorphisim using animals as a parent class

---
+++ OOP/polymorphisim/Runtime.java
@@ -0,0 +1,45 @@
+package OOP.polymorphisim;
+
+import java.util.Scanner;
+
+public class Runtime {
+    public static void main(String[] args) {
+        int choice;
+        try(Scanner scanner = new Scanner(System.in)){
+            System.out.print("would you like a dog or a car?(1 for dog/2 for cat): ");
+            choice = scanner.nextInt();
+        }
+        Animal animal;
+        if(choice ==1){
+            animal = new Dog();
+            animal.speak();
+        }
+        else if (choice ==2){
+            animal = new Cat();
+            animal.speak();
+        }
+
+        
+    }
+    
+}
+
+abstract class Animal{
+    abstract void speak();
+}
+
+class Dog extends Animal{
+
+    @Override
+    void speak(){
+        System.out.println("the dog Braks baw baw!!");
+    }
+}
+
+class Cat extends Animal{
+
+    @Override
+    void speak(){
+        System.out.println("the cat goes meow moew!!");
+    }
+}
