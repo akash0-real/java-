f788255200b85721b455606d415732b7f4e93f70
akash <aayush45r@gmail.com>
2025-09-26 23:36:15 +0530

    Created mutliple threads and used join

--- OOP/threads/Multi.java
+++ OOP/threads/Multi.java
@@ -1,5 +1,35 @@
 package OOP.threads;
 
 public class Multi {
+   public static void main(String[] args) {
+    Thread thread = new Thread(new Num("akash"));
+    Thread thread1 = new Thread(new Num("Ayush"));
+    System.out.println("Game start!");
+    thread.start();
+    thread1.start();
+    try {
+        thread.join();
+        thread1.join();
+    } catch (InterruptedException e) {
+        System.out.println("OPPS! something went wrong....");
+    }
+    System.out.println("Game over!");
     
+   } 
+}
+
+class Num implements Runnable{
+    private final String name;
+
+    Num(String name) {
+        this.name = name;
+    }
+    
+
+    @Override
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println(name);
+        }
+    }
 }
